88c1506d5a693be76ceb57c690f4eecb70f61b43
Jimmy Muller <jmuller3140@gmail.com>
2018-05-03 00:09:29 -0400

    added registration clear

--- client/src/routes/Register.js
+++ client/src/routes/Register.js
@@ -28,21 +28,28 @@ import Header from '../components/Header';
          const { email, password, firstName, lastName } = this.state;
          const data = {email: email, password: password, firstName: firstName, lastName: lastName};
       e.preventDefault();
+
+      let that=this;
       request
     .post('http://localhost:3001/register')
     .set('Content-Type', 'application/json')
     .set('Accept', 'application/json')
     .send( data )
     .end(function(err, res){
+      that.setState({email: ''});
+      that.setState({password: ''});
+      that.setState({firstName: ''});
+      that.setState({lastName: ''});
       console.log(res);
     });  
 }
 
    render(){
+    const props = {handleSubmit: this.handleSubmit, onChange: this.onChange, email: this.state.email, password: this.state.password, firstName: this.state.firstName, lastName: this.state.lastName};
      return (
       <div>
         <Header pageName="Register"/>
-        <RegisterComponent handleSubmit={this.handleSubmit} onChange={this.onChange}/>
+        <RegisterComponent {...props}/>
       </div>
       )
    }
